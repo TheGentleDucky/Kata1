4329340031fb10511fc3e3cd3543487821dc94a8
TheGentleDucky <owenscruzsean@gmail.com>
2025-10-20 10:35:50 +0100

    Kata 1 terminada

--- src/software/ulpgc/kata1/Main.java
+++ src/software/ulpgc/kata1/Main.java
@@ -1,7 +1,10 @@
 package software.ulpgc.kata1;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-
+        Esqueleto Marcelo = new Esqueleto("Marcelo", 206, LocalDate.of(1999,4,1));
+        System.out.println(Marcelo.getName() + "nació el " + Marcelo.getBirthdate() + ", tiene "+ Marcelo.getAge() + " años y conserva "+ Marcelo.getNumHuesos() + " huesos.");
     }
 }
