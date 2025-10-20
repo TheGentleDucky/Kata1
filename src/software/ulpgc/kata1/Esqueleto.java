156fd637fcc2311cdef817a7368bc4bf57ad1acc
TheGentleDucky <owenscruzsean@gmail.com>
2025-10-20 10:30:27 +0100

    Kata 1, Esqueleto.java hecho

--- src/software/ulpgc/kata1/Esqueleto.java
+++ src/software/ulpgc/kata1/Esqueleto.java
@@ -1,4 +1,35 @@
 package software.ulpgc.kata1;
 
+import java.time.LocalDate;
+
 public class Esqueleto {
+    private final String name;
+    private final int numHuesos;
+    private final LocalDate birthdate;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumHuesos() {
+        return numHuesos;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public Esqueleto(String name, int numHuesos, LocalDate birthdate) {
+        this.name = name;
+        this.numHuesos = numHuesos;
+        this.birthdate = birthdate;
+    }
+
+    public int getAge(){
+        return toYears(LocalDate.now().toEpochDay() - birthdate.toEpochDay());
+    }
+
+    public int toYears(long days){
+        return(int)(days/365.2);
+    }
 }
