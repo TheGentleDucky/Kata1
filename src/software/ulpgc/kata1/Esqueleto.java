384367b2a23253c93299e301a51384b00916faf2
TheGentleDucky <owenscruzsean@gmail.com>
2025-10-20 10:25:45 +0100

    Kata 1 ppuesta para grabar 20/10/2025

--- src/software/ulpgc/kata1/Esqueleto.java
+++ src/software/ulpgc/kata1/Esqueleto.java
@@ -1,35 +1,4 @@
 package software.ulpgc.kata1;
 
-import java.time.LocalDate;
-
 public class Esqueleto {
-    private final String name;
-    private final LocalDate birthdate;
-    private final int numHuesos;
-
-    public Esqueleto(String name, LocalDate birthdate, int numHuesos) {
-        this.name = name;
-        this.birthdate = birthdate;
-        this.numHuesos = numHuesos;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public LocalDate getBirthdate() {
-        return birthdate;
-    }
-
-    public int getNumHuesos() {
-        return numHuesos;
-    }
-
-    public int getAge() {
-        return toYears(LocalDate.now().toEpochDay() - birthdate.toEpochDay());
-
-    }
-    public int toYears(long days){
-        return(int)(days/365.2);
-    }
 }
--- src/software/ulpgc/kata1/Main.java
+++ src/software/ulpgc/kata1/Main.java
@@ -1,10 +1,7 @@
 package software.ulpgc.kata1;
 
-import java.time.LocalDate;
-
 public class Main {
     public static void main(String[] args) {
-        Esqueleto marcelo= new Esqueleto("Marecelo", LocalDate.of(1999, 4, 1), 206);
-        System.out.println(marcelo.getName() + " nació el  " + marcelo.getBirthdate() + ", por lo que tiene " + marcelo.getAge() + " años y cuenta con " + marcelo.getNumHuesos() + " huesos.");
+
     }
 }
