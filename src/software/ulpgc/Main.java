67f1562ed1f4ec3fda9f0a6d08027d660db8f4d0
TheGentleDucky <owenscruzsean@gmail.com>
2025-10-20 12:14:09 +0100

    Kata 1 terminada por séptima vez

--- src/software/ulpgc/Main.java
+++ src/software/ulpgc/Main.java
@@ -1,7 +1,10 @@
 package software.ulpgc;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-
+    Esqueleto Juan = new Esqueleto("Juan", 205, LocalDate.of(2002, 5,  21));
+    System.out.println(Juan.getName() + " nació el  " + Juan.getBirthdate() + ", por lo que tiene " + Juan.getAge() + " años y conserva " + Juan.getNumHuesos() + " huesos intactos.");
     }
 }
